cb51baee546e95a3c47b6bbb94ecc13d0229fcee
jayantak <jayanta.k@outlook.com>
2015-06-18 15:10:40 +0530

    Second Test

--- src/main/webapp/js/test/test.spec.js
+++ src/main/webapp/js/test/test.spec.js
@@ -1,28 +1,27 @@
 define(function(require)
 {
     var forms = require('../FormAdd.js');
-    it('Element should be added', function()
-    {
-        assert.equal("hello", "hello");
-        var items1 = new Object();
+    it('Carousel element should be added', function() {
+        var items1 = {};
         items1.carouselItems = [];
-        items1.formItems = [];
         var A = "TestCarouselItem";
         var B = {
             "set" : "carousel",
             "type" : "image",
             "file" : "img.jpg"
         };
+        forms.addElement(items1, A, B);
+        assert.equal(items1.carouselItems[0], '<div class="item"><img class="pure-img" src="images/'+B["file"]+'"></div>');
+    });
+    it('Form element should be added', function() {
+        var items1 = {};
+        items1.formItems = [];
         var C = "TestFormItem";
         var D = {
             "set" : "form",
             "type" : "text"
         };
         forms.addElement(items1, C, D);
-        forms.addElement(items1, A, B);
-        console.log(items1[0]);
         assert.equal(items1.formItems[0],'<label for="' + C + '">' + C + '</label><input name ="' + C + '" type="' + D["type"] + '"></input>');
-        assert.equal(items1.carouselItems[0], '<div class="item"><img class="pure-img" src="images/'+B["file"]+'"></div>');
-        assert.equal("hello", "hello");
     })
 });
