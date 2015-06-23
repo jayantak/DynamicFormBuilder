ad99d429ac1c3372f4bbeed34d1844d5ac811136
jayantak <jayanta.k@outlook.com>
2015-06-23 09:31:59 +0530

    Checking JUnit tests on Snap2

--- src/main/webapp/js/test/HomePageFunctionsTest.spec.js
+++ src/main/webapp/js/test/HomePageFunctionsTest.spec.js
@@ -1,8 +1,8 @@
 define(['HomePageFunctions'], function(Page){
 
     it('POST HTTPRequest must succeed', function(){
-
-        var dummyElement = document.createElement('div');
-        document.getElementById = jasmine.createSpy('HTML Element').return(dummyElement);
+        //
+        //var dummyElement = document.createElement('div');
+        //document.getElementById = jasmine.createSpy('HTML Element').return(dummyElement);
     })
 });
