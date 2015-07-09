42047bbc0b83ba664710b9946e01ffa739ccba7d
jayantak <jayanta.k@outlook.com>
2015-07-09 11:21:55 +0530

    Added tomcat plugin

--- src/main/java/com/springapp/mvc/FormController.java
+++ src/main/java/com/springapp/mvc/FormController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 @Controller
