adbe17f716815b7779e94a6a790a9dd597d3123e
jayantak <jayanta.k@outlook.com>
2015-07-13 11:24:45 +0530

    test for currentForm

--- src/main/webapp/js/test/currentFormTest.spec.js
+++ src/main/webapp/js/test/currentFormTest.spec.js
@@ -1,6 +1,15 @@
-define(['currentForm'], function(){
+define(['currentForm'], function(Form){
 
     it('addElement should be successful', function(){
-        
+
+
+        append = sinon.stub($.fn, "append");
+
+        Form.addElement("KeyName");
+
+        assert(append.calledOnce);
+        append.thi
+
+        append.restore();
     })
 })
