3d20c7c2afbe71c6f26d9f4f59cf4fe0f20249f6
Nsudeep <sudeepna@thoughtworks.com>
2015-07-24 12:02:53 +0530

    Validations except 'necessar' complete on server side but not tested.

--- src/main/java/com/dfb/Validation.java
+++ src/main/java/com/dfb/Validation.java
@@ -18,6 +18,15 @@ public class Validation {
 
     JSONOperations jsonOperations = new JSONOperations("dataFields.json", "dataOut.json");
 
+    public boolean patternchecker(String patternstr, String input)
+    {
+        Pattern pattern = Pattern.compile(patternstr);
+
+
+        Matcher matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
     public int check(MongoOperations mongoOperations, Map userData) throws IOException {
 
         Collection formFields1=userData.keySet();
@@ -30,11 +39,12 @@ public class Validation {
 
 
 
+        boolean matchFound;
         int i;
-        String temp,maxi,input,value;
+        String temp,maxi,input,value,type;
         Object objattr;
         JSONObject jobjattr;
-        String formFields,patternstr;
+        String formFields,patternstr,inputval;
         JSONObject attribute;
         JSONObject formStructure1;
         JSONOperations jops1 = new JSONOperations("dataFields.json", "dataOut.json");
@@ -80,20 +90,10 @@ public class Validation {
                         maxi = jobjattr.get("maxlength").toString();
                         float maxf= Float.parseFloat(maxi);
 
+                        inputval= getInput(formFields1,formFields,formvalues);
 
-                      //  System.out.println("maxi is " + maxi);
-                        i=0;
-                        while(1>-1)
-                        {
-                            if(formFields1.toArray()[i].equals(formFields))
-                            {
-                                break;
-                            }
-                        }
 
-                        value = formvalues.toArray()[i].toString();
-
-                        if(value.length()>maxf)
+                        if(inputval.length()>maxf)
                             System.out.println("Value in " + formFields+" is too large");
 
                     }
@@ -101,32 +101,46 @@ public class Validation {
                     if(jobjattr.containsKey("pattern"))
                     {
                         patternstr = jobjattr.get("pattern").toString();
-                        Pattern pattern = Pattern.compile(patternstr);
-                        i=0;
-                        while(1>-1)
-                        {
-                            if(formFields1.toArray()[i].equals(formFields))
-                            {
-                                break;
-                            }
-                        }
-                        value = formvalues.toArray()[i].toString();
-                        Matcher matcher = pattern.matcher(value);
-                        boolean matchFound = matcher.matches();
+                        inputval= getInput(formFields1,formFields,formvalues);
+                        matchFound = patternchecker(patternstr, inputval);
                         if(!matchFound)
                             System.out.println("Value at " + formFields+ " does not obey the pattern");
+                    }
+                    if(jobjattr.containsKey("type"))
+                    {
+                        type = jobjattr.get("type").toString();
+                        if(type.equals("email"))
+                        {
+                            patternstr = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+                            inputval= getInput(formFields1,formFields,formvalues);
+                            matchFound = patternchecker(patternstr, inputval);
+                            if (!matchFound)
+                                System.out.println("Invalid email ID");
+                        }
+
+                        if(type.equals("date")) {
+                            patternstr = "(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)";
+                            inputval= getInput(formFields1,formFields,formvalues);
+                            matchFound = patternchecker(patternstr, inputval);
+
+                            if(!matchFound)
+                                System.out.println("Invalid email ID");
+
+                        }
 
 
-                        
-
+                    }
+                    if(jobjattr.containsKey("validation"))
+                    {
+                        String validations = jobjattr.get("validation").toString();
                     }
 
                 }
 
 
             }
-            catch(Exception e)
-            {
+            catch(Exception e) {
                 System.out.println("error is " + e.getMessage());
             }
 
@@ -135,6 +149,21 @@ public class Validation {
 
 
         return 1;
+    }
+
+    public String getInput(Collection formFields1, String formFields, Collection formvalues) {
+
+        int i=0;
+        while(true)
+        {
+            if(formFields1.toArray()[i].equals(formFields))
+            {
+                break;
+            }
+            i++;
+        }
+
+        return formvalues.toArray()[i].toString();
     }
 
 }
